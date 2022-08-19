d8bc0ef4b4637ca2e77f2d2ab898e87b0b1ee91e
Peter Koller <peterkollerlv@live.com>
2022-08-19 18:15:01 +0200

    fix(*): minor adjustments

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,17 @@
 import typescript from "@rollup/plugin-typescript";
 
 export default {
-  input: "./packages/consumer-library/src/index.ts",
-  plugins: [typescript()],
+  input: "packages/consumer-library/src/index.ts",
+  plugins: [
+    typescript({
+      noEmit: false,
+      declaration: true,
+      rootDir: "packages/consumer-library/src",
+      declarationDir: "packages/consumer-library/dist",
+    }),
+  ],
   output: {
-    file: "./packages/consumer-library/dist/index.js",
+    file: "packages/consumer-library/dist/index.js",
     format: "esm",
     name: "bundle",
   },
